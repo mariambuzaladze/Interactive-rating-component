70f756f706bc76eb0a2ec6a07d0fb03a6c43928d
mariambuzaladze <mariambuzaladze10@example.come>
2024-03-07 15:49:15 +0400

    fix and update buttons

--- src/components/Buttons/Buttons.jsx
+++ src/components/Buttons/Buttons.jsx
@@ -5,6 +5,11 @@ export default function Buttons(props) {
     backgroundImage: `url(${props.img})`,
     backgroundRepeat: "no-repeat",
     backgroundPosition: "center",
+    width: `${props.width}px`,
+    height: `${props.height}px`,
+    backgroundColor: `${props.backgroundColor}`,
+    color: `${props.textColor}`,
+    borderRadius: `${props.borderRadius}`,
   };
 
   return <button style={buttonStyle}>{props.text}</button>;
--- src/components/Rating/Rating.jsx
+++ src/components/Rating/Rating.jsx
@@ -5,10 +5,42 @@ import Star from "/images/icon-star.svg";
 
 export default function Rating() {
   const [img, setImg] = useState(Star);
-  const [text, setText] = useState(1);
+  const [text, setText] = useState("");
+  const [height, setHeight] = useState(40);
+  const [width, setWidth] = useState(40);
+  const [backgroundColor, setBackgroundColor] = useState("#262e38");
+  const [textColor, setTextColor] = useState("#7c8798");
+  const [borderRadius, setBoderRadius] = useState("50%");
+
+  function displayButtons() {
+    const buttons = [];
+    for (let i = 1; i < 6; i++) {
+      buttons.push(
+        <Buttons
+          text={i}
+          img=""
+          height="42"
+          width="42"
+          backgroundColor={backgroundColor}
+          textColor={textColor}
+          borderRadius={borderRadius}
+        />
+      );
+    }
+    return buttons;
+  }
+
   return (
     <div className="container">
-      <Buttons text="" img={img} />
+      <Buttons
+        text={text}
+        img={img}
+        height={height}
+        width={width}
+        backgroundColor={backgroundColor}
+        textColor={textColor}
+        borderRadius={borderRadius}
+      />
 
       <div className="text">
         <h1>How did we do?</h1>
@@ -18,13 +50,16 @@ export default function Rating() {
         </p>
       </div>
 
-      <div className="button">
-        <Buttons text="1" img="" />
-        <Buttons text="2" img="" />
-        <Buttons text="3" img="" />
-        <Buttons text="4" img="" />
-        <Buttons text="5" img="" />
-      </div>
+      <div className="button">{displayButtons()}</div>
+      <Buttons
+        text="SUBMIT"
+        img=""
+        height="45"
+        width="279"
+        backgroundColor="#fc7614"
+        textColor="#fff"
+        borderRadius="22.5px"
+      />
     </div>
   );
 }
