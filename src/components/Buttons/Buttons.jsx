84517804fdec88a99b870d3a3f3724bafd055371
mariambuzaladze <mariambuzaladze10@example.come>
2024-03-07 18:17:39 +0400

    fix button styles and functionality

--- src/components/Buttons/Buttons.jsx
+++ src/components/Buttons/Buttons.jsx
@@ -1,16 +1,6 @@
 import "./buttons.css";
+import Star from "/images/icon-star.svg";
 
 export default function Buttons(props) {
-  const buttonStyle = {
-    backgroundImage: `url(${props.img})`,
-    backgroundRepeat: "no-repeat",
-    backgroundPosition: "center",
-    width: `${props.width}px`,
-    height: `${props.height}px`,
-    backgroundColor: `${props.backgroundColor}`,
-    color: `${props.textColor}`,
-    borderRadius: `${props.borderRadius}`,
-  };
-
-  return <button style={buttonStyle}>{props.text}</button>;
+  return <button className={`${props.className}`}>{props.text}</button>;
 }
--- src/components/Rating/Rating.jsx
+++ src/components/Rating/Rating.jsx
@@ -1,46 +1,22 @@
 import { useState } from "react";
 import "./rating.css";
 import Buttons from "../Buttons/Buttons";
-import Star from "/images/icon-star.svg";
 
 export default function Rating() {
-  const [img, setImg] = useState(Star);
   const [text, setText] = useState("");
-  const [height, setHeight] = useState(40);
-  const [width, setWidth] = useState(40);
-  const [backgroundColor, setBackgroundColor] = useState("#262e38");
-  const [textColor, setTextColor] = useState("#7c8798");
-  const [borderRadius, setBoderRadius] = useState("50%");
+  const [className, setClassName] = useState("button");
 
   function displayButtons() {
     const buttons = [];
     for (let i = 1; i < 6; i++) {
-      buttons.push(
-        <Buttons
-          text={i}
-          img=""
-          height="42"
-          width="42"
-          backgroundColor={backgroundColor}
-          textColor={textColor}
-          borderRadius={borderRadius}
-        />
-      );
+      buttons.push(<Buttons key={i} text={i} className="number" />);
     }
     return buttons;
   }
 
   return (
     <div className="container">
-      <Buttons
-        text={text}
-        img={img}
-        height={height}
-        width={width}
-        backgroundColor={backgroundColor}
-        textColor={textColor}
-        borderRadius={borderRadius}
-      />
+      <Buttons text={text} className={className} />
 
       <div className="text">
         <h1>How did we do?</h1>
@@ -51,15 +27,7 @@ export default function Rating() {
       </div>
 
       <div className="button">{displayButtons()}</div>
-      <Buttons
-        text="SUBMIT"
-        img=""
-        height="45"
-        width="279"
-        backgroundColor="#fc7614"
-        textColor="#fff"
-        borderRadius="22.5px"
-      />
+      <Buttons text="SUBMIT" className="submit" />
     </div>
   );
 }
