9a8e70ed076b20ad70695fa18805d895ad2d5cfb
mariambuzaladze <mariambuzaladze10@example.come>
2024-03-07 15:13:27 +0400

    add and style button component

--- src/components/Buttons/Buttons.jsx
+++ src/components/Buttons/Buttons.jsx
@@ -1,5 +1,11 @@
 import "./buttons.css";
 
 export default function Buttons(props) {
-  return <></>;
+  const buttonStyle = {
+    backgroundImage: `url(${props.img})`,
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "center",
+  };
+
+  return <button style={buttonStyle}>{props.text}</button>;
 }
--- src/components/Rating/Rating.jsx
+++ src/components/Rating/Rating.jsx
@@ -1,7 +1,14 @@
 import { useState } from "react";
 import "./rating.css";
 import Buttons from "../Buttons/Buttons";
+import Star from "/images/icon-star.svg";
 
 export default function Rating() {
-  return <></>;
+  const [img, setImg] = useState(Star);
+  const [text, setText] = useState(1);
+  return (
+    <div className="container">
+      <Buttons text="" img={img} />
+    </div>
+  );
 }
